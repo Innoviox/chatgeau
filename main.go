558706a1b52a733bbf990cdc782693d2a17f1293
Simon Chervenak <sarras305@gmail.com>
2020-12-25 15:31:18 -0500

    add some lighting

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/g3n/engine/light"
+	"github.com/g3n/engine/math32"
 	"io/ioutil"
 	"strings"
 
 	"github.com/g3n/engine/core"
-	"github.com/g3n/engine/graphic"
-	"github.com/g3n/engine/light"
 	"github.com/g3n/engine/loader/obj"
-	"github.com/g3n/engine/math32"
 	"github.com/g3n/engine/util/application"
 )
 
@@ -38,16 +37,27 @@ func loadModel(path string) *core.Node {
 }
 
 func loadLevel(app *application.Application, path string) error {
-	dat, err := ioutil.ReadFile(path)
+	dat, err := ioutil.ReadFile("resources/levels/"+path+".txt")
 	if err != nil {
 		return err
 	}
 
+	pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 8.0)
+
 	for i, row := range strings.Split(string(dat), "\n") {
 		for j, char := range row {
 			m := loadModel(models[char])
+
+			app.Scene().Add(m)
+			m.SetPosition(float32(i), 0, float32(j))
+
+			pointLight.SetPosition(float32(i / 2), 3, float32(j / 2))
 		}
 	}
+
+	app.Scene().Add(pointLight)
+
+	return nil
 }
 
 func main() {
@@ -58,20 +68,15 @@ func main() {
 	})
 
 	// Add lights to the scene
-	ambientLight := light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8)
-	app.Scene().Add(ambientLight)
-	pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 5.0)
-	pointLight.SetPosition(1, 0, 2)
-	app.Scene().Add(pointLight)
 
 	// Add an axis helper to the scene
-	axis := graphic.NewAxisHelper(0.5)
-	app.Scene().Add(axis)
+	//axis := graphic.NewAxisHelper(0.5)
+	//app.Scene().Add(axis)
 
 	if err := loadLevel(app, "forest"); err != nil {
 		fmt.Println(err)
 	}
 
-	app.CameraPersp().SetPosition(0, 0, 3)
+	app.CameraPersp().SetPosition(0, 1, 0)
 	app.Run()
 }
